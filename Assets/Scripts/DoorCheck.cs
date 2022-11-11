559275d17a9951e6ec465df72c0fba7f3a4bca79
Kiing_ot <oluwatosinogunyebi@gmail.com>
2022-11-11 04:02:28 +0000

    Added Animations

---
+++ Assets/Scripts/DoorCheck.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class DoorCheck : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            anim.SetTrigger("Revolve");
+        }
+
+    }
+}
